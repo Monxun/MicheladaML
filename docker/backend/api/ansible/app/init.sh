a97668c4a471ecfc6d0a83d01b720684bc623c15
Michael Gibney <michaelgibney1@gmail.com>
2022-06-12 21:30:19 -0700

    updated vars

--- docker/backend/api/ansible/app/init.sh
+++ docker/backend/api/ansible/app/init.sh
@@ -4,9 +4,9 @@ echo "INITIALIZING DJANGO API..."
 
 ls
 
-if [[ ! -d "./core/api/services" ]]
-then
-    echo "MOVING SERVICES..."
-    mv ./services ./core/api
-    exit
-fi
+# if [[ ! -d "./core/api/services" ]]
+# then
+#     echo "MOVING SERVICES..."
+#     mv ./services ./core/api
+#     exit
+# fi
